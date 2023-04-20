587285b1fa6609d3b1fe269717ac7940a05b0db8
Ben George <93751310+btangatakino@users.noreply.github.com>
2023-04-20 09:51:35 +0000

    Added another print to console.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,10 @@
 using System;
-
+//add comment
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("I just figured out something.");
     }
 }
