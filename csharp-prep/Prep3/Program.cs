adbb76017f53c823cbfa1e19ec13dcd0df4f17ce
bengeorge70 <btangatakino@gmail.com>
2023-04-21 02:10:18 -1000

    Change Prep3 to guess number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNumber = new Random();
+        int number = randomNumber.Next(1, 11);
+
+        string response;
+
+        do
+        {
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while (response == "yes");
     }
 }
