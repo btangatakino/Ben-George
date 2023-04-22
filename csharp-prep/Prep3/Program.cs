f2f180a4f055a7b07af8a79b238ae5ba5738370f
bengeorge70 <btangatakino@gmail.com>
2023-04-21 21:25:16 -1000

    modify Prep3 for guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,15 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomNumber = new Random();
-        int number = randomNumber.Next(1, 11);
-
-        string response;
-
-        do
+        string response4 = "yes";
+        while (response4 == "yes")
         {
-            Console.Write("Do you want to continue? ");
-            response = Console.ReadLine();
-        } while (response == "yes");
+            Console.Write("What is the magic number? ");
+            string response = Console.ReadLine();
+            int magicNumber = int.Parse(response);
+
+            /*Console.Write("What is your guess? ");
+            string response2 = Console.ReadLine();
+            int guess = int.Parse(response2);*/
+
+            int guess = -1;
+                    
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string response2 = Console.ReadLine();
+                guess = int.Parse(response2);
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+            Console.Write("Do you want to continue?" );
+            response4 = Console.ReadLine();
+        }
+        /*Random randomNumber = new Random();
+        int number = randomNumber.Next(1, 101);*/
+
     }
 }
