76214cc871da3559124f1095466b14b0148d10dd
Ben George <btangatakino@gmail.com>
2023-04-26 09:50:56 -1000

    Corrected some typing error on line 10

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -7,7 +7,7 @@ public class Job
     public int _endYear;
     public string _company;
 
-    public void DisplayJobDetailsgit()
+    public void DisplayJobDetails()
     {
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
