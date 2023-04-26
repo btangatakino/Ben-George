4d2f47db56416a5a45e6646132da1d07fa173cd4
Ben George <btangatakino@gmail.com>
2023-04-26 00:19:40 -1000

    created Job.cs, Resume.cs n modified Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // new Job instance named job1
+        Job job1 = new Job();
+
+        // setting member variables to job1
+        job1._jobTitle = "Sofware Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        //create another Job instance named job2
+        Job job2 = new Job();
+
+        //set member variable to job2
+        job2._jobTitle = "Manager";
+        job1._company = "Apple";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        //create new Resume instance named myResume
+        Resume myResume = new Resume();
+
+
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        //myResume.DisplayJobDetails();
+
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
     }
 }
