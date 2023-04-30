e2f16e193e0df9434d1e4b725d59bb8bc3d4ff96
Ben George <btangatakino@gmail.com>
2023-04-29 16:20:02 -1000

    added comments

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,5 +1,3 @@
-using System;
-
 /*This program is to help people record events of their day
 by supplyinf prompts and then saving their responses along 
 with the question and the date to a file */
