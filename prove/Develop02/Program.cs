c2b0033ff1bb37c0d27026928f27b84052ab3f6d
Ben George <btangatakino@gmail.com>
2023-05-04 23:08:59 -1000

    added Journal Classwith DisplayWelcomMessage and PromptUserInput Methods

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,17 +4,40 @@ using System;
 by supplying prompts and then saving their responses along 
 with the question and the date to a file */
 
-class Program
+class Journal
 {
     //adding comments
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        DisplayWelcomeMessage();
 
-        class Journal
-        {
-            //some method here
-            private List<Entry> _entries = new List<Entry>();
+        //string _userInput = PromptUserInput();
+        string _fileName;
+        string _randomPrompt;
+        string _promptResponse;
+    }
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.WriteLine("Please select one of the following choices:");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        
+    }
+
+    static int PromptUserInput()
+    {
+        Console.WriteLine("What would you like to do?");
+        int userInput = int.Parse(Console.ReadLine());
+        return userInput;
+    }
+}
+        /*
+        //some method here
+        private List<Entry> _entries = new List<Entry>();
         }
 
         class Entry
@@ -27,4 +50,4 @@ class Program
             //some methods here
         }
     }
-}
+}*/
