47119e707d22ea337b30f6fc83f1f7bd30752487
Ben George <btangatakino@gmail.com>
2023-05-05 22:36:33 -1000

    changed something here but can't remember

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -2,13 +2,16 @@ using System;
 
 public class Entry
 {
-    public string _listOfEntries;
+    //public string _listOfEntries;
 
-    public string _dateOfEntry = "29th April 1970";
+    //public string _dateOfEntry = "29th April 1970";
+    public string _dateOfEntry;
 
-    public string _promptResponse = "I was born on that day.";
+    //public string _promptResponse = "I was born on that day.";
+    public string _promptResponse;
 
-    public string _promptQuestion = "What interesting event happened to you on that day?" ;
+    //public string _promptQuestion = "What interesting event happened to you on that day?" ;
+    public string _promptQuestion;
     
     public void Display()
     {
