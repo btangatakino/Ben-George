62c7c50ac6808cd784bf2bcdd5152bdf7b459fe5
Ben George <btangatakino@gmail.com>
2023-05-06 23:49:12 -1000

    reset PromptGenerator class

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -3,14 +3,8 @@ using System.Collections.Generic;
 
 public class PromptGenerator
 {
+
     List<String> _prompts = new List<String>();
 
-    _prompts.Add("What is one memorable thing that happened today");
-    _prompts.Add("What is one memorable thing that happened today");
-
-    foreach(String item in _prompts)
-    {
-        Console.WriteLine(item);
-    }
-    Console.ReadKey();
+    
 }
