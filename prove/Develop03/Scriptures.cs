704231882d15eeaae12d9d12957d0e563b4e97e4
Ben George <btangatakino@gmail.com>
2023-05-19 01:54:14 -1000

    added a list of scriptures

--- prove/Develop03/Scriptures.cs
+++ prove/Develop03/Scriptures.cs
@@ -19,6 +19,11 @@ public class Scriptures
     Scriptures script2 = new Scriptures(reference, text);
     Scriptures script3 = new Scriptures(reference, text);
 
+    List<string> scriptures = new List<string>();
+        scriptures.Add("James 1:5 If any of you lack wisdom, let him ask of God, that giveth to all men liberally, and upbraideth not; and it shall be given him.");
+        scriptures.Add("Proverbs 3:5-6 Trust in the Lord with all your heart, and lean not to your own understanding. In all thy ways, acknowledge Him, and He shall direct thy paths.");
+        scriptures.Add("Moses 1:39 For behold, this is my work and my glory, to bring to pass the imortality and eternal life of man.");
+
     }
     //method to hide words
     public void HideWords()
