a8c011245ff90db9f6b160415bac7167eceea697
Ben George <btangatakino@gmail.com>
2023-05-19 01:07:02 -1000

    added 3 new Scriptures objects, script1, script2, and script3

--- prove/Develop03/Scriptures.cs
+++ prove/Develop03/Scriptures.cs
@@ -5,6 +5,16 @@ public class Scriptures
     private string _reference;
     private string _text;
 
+    Scriptures(string reference, string text)
+    {
+        _reference = reference;
+        _text = text;
+    
+    Scriptures script1 = new Scriptures(reference, text);
+    Scriptures script2 = new Scriptures(reference, text);
+    Scriptures script3 = new Scriptures(reference, text);
+    }
+
     //method to hide words
     public void HideWords()
     {
