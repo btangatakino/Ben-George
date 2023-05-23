a378c814c021199b32f31c590258f1ffc07a24f6
Ben George <btangatakino@gmail.com>
2023-05-23 08:39:47 -1000

    addede Bank.cs code

---
+++ encapsulation/Bank.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace EncapsulationDemo
+{
+    public class Bank
+    {
+        private int Amount;
+        public int GetAmount()
+        {
+            return Amount;
+        }
+        public void SetAmount(int Amount)
+        {
+            if (Amount > 0)
+            {
+                this.Amount = Amount;
+            }
+            else
+            {
+                throw new Exception("Please Pass a Positive Value");
+            }
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                Bank bank = new Bank();
+                //cannot access the Amount Variable directly
+                //bank.Amount = 50; --> Complie Time Error
+                bank.SetAmount(10);
+                Console.WriteLine(bank.GetAmount());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }   
+    }
+}
