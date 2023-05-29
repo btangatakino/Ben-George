0b7bc4a6b0cc39b3a6c2e30e97f81bac985a066a
Ben George <btangatakino@gmail.com>
2023-05-29 00:57:02 -1000

    New MathAssignment child class

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+    public  MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        /*string studentName = GetStudentName();*/
+        return ($"Section {_textbookSection} Problems {_problems}");
+    }
+}
