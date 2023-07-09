31014a5438daa84516936901c71318ded6e70ea6
Ben <ben.tangatakino@gmail.com>
2023-07-09 03:28:00 -1000

    added comment for video

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,13 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
-
         // creating new Video instances
         Video video1 = new Video("You are the reason", "Calum Scott", 204);
         Video video2 = new Video("", "", 10);
         Video video3 = new Video("", "", 10);
 
-
+        // creating new Comment instances
+        Comment v1Comment1 = new Comment("Jeferson Zucoloto", "Such a beautiful song. I've just got home, I was in London last December. This song makes me remember everything I felt there.");
+        Comment v1Comment2 = new Comment("Fabricio Meneses", "Can't get enough of this song. Love it so much.");
+        Comment v1Comment3 = new Comment("Tiara Kohlisch", "It's not only the melody and the lyrics which brought me to tears,  this music video is a masterpiece too.");
     }
 }
