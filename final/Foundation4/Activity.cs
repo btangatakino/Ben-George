0604379fd970fd75b7116e4fd5fba58e497f84d0
Ben <ben.tangatakino@gmail.com>
2023-07-12 01:43:22 -1000

    added classes for Foundation4 code

---
+++ final/Foundation4/Activity.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+public class Activity
+{
+    private dateType _date;
+    private double _time;
+    private double _length;
+
+    private class dateType
+    {
+    }
+
+    public abstract Distance()
+    {
+
+    }
+
+    public abstract Speed()
+    {
+
+    }
+
+    public abstract Pace()
+    {
+
+    }
+}
