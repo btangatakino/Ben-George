58e50089ad5ae58a6bd2ea87254a3f96b33f3607
Ben <ben.tangatakino@gmail.com>
2023-07-15 16:23:45 -1000

    addend content

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -5,9 +5,20 @@ public class Event
 {
     protected string _eventTitle;
     protected string _description;
-    protected DateTime _date;
+    protected string _date;
     protected string _time;
     protected string _address;
+    protected string _eventType;
+    protected string _eventInfor;
+
+    public Event(string eventTitle, string description, string date, string time, string address)
+    {
+        _eventTitle = eventTitle;
+        _description = description;
+        _date = date;
+        _time = time;
+        _address = address;
+    }
 
     public void StandardDetails()
     {
@@ -16,11 +27,11 @@ public class Event
 
     public void FullDetails()
     {
-        Console.WriteLine();
+        Console.WriteLine($"{_eventTitle}, {_description}, {_date}, {_time}, {_address}, {_eventType}, {_eventInfor}");
     }
 
     public void ShortDescription()
     {
-        Console.WriteLine();
+        Console.WriteLine($"{_eventType}, {_eventTitle}, {_date}");
     }
 }
--- final/Foundation3/Lectures.cs
+++ final/Foundation3/Lectures.cs
@@ -6,8 +6,9 @@ public class Lectures : Event
     private string _lecturer;
     private int _capacity;
 
-    public Lectures(string lecture, string capacity)
+    public Lectures(string lecturer, int capacity) : base(eventTitle, description, date, time, address)
     {
-
+        _lecturer = lecturer;
+        _capacity = capacity;
     }
 }
